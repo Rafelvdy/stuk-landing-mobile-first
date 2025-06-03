192c9668aef1c773a2c06fda1522a07fbbaa7c44
Rafe Loveday <rafe.loveday@icloud.com>
2025-06-03 03:20:48 +0100

    themes aesthetic changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -398,8 +398,12 @@ export default function Home() {
             <div className={styles.ThemesGraphic}></div>
             <h2 className={styles.ThemesGraphicSubtitle}>Each day at Startup Village is themed around a different frontier of tech.</h2>
             <div className={styles.ThemesXtraInfo}>
-              <p>Dive into our interactive digital gallery, test your knowledge with a daily quiz, and catch two expert speakers sharing real insights and future-facing ideas.</p>
-              <p>Check back daily for new content, new talks, new opportunities.</p>
+              <div className={styles.ThemesXtraInfoTextList}>
+                <p>- Dive into our interactive digital gallery</p>
+                <p>- Test your knowledge with a daily quiz</p>
+                <p>- Catch two expert speakers sharing real insights and future-facing ideas.</p>
+              </div>
+              <p className={styles.ThemesXtraInfoText}>Check back daily for new content, new talks, new opportunities.</p>
             </div>
           </div>
 
